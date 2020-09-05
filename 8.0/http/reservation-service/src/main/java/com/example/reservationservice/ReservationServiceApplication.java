c9b8c53233d5383b903fdfd8c26cadfc0a219ea9
Frank <archerfrank@gmail.com>
2020-09-05 16:19:31 +0800

    gateway fix

--- 8.0/http/reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ 8.0/http/reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -1,13 +1,16 @@
 package com.example.reservationservice;
 
+import io.r2dbc.spi.ConnectionFactory;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.r2dbc.core.DatabaseClient;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -19,22 +22,30 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
+@AllArgsConstructor
 public class ReservationServiceApplication {
+
 
   public static void main(String[] args) {
     SpringApplication.run(ReservationServiceApplication.class, args);
   }
-
+  private DatabaseClient databaseClient;
   @Bean
-  RouterFunction<ServerResponse> routes(ReservationRepository rr) {
+  RouterFunction<ServerResponse> routes() {
     return route()
-        .GET("/reservations", r -> ok().body(rr.findAll(), Reservation.class))
-        .build();
+            .GET("/reservations", r -> ok().body(databaseClient.select()
+                    .from("reservation")
+                    .as(Reservation.class).fetch().all(),Reservation.class))
+            .build();
   }
+
 }
 
-interface ReservationRepository extends ReactiveCrudRepository<Reservation, Integer> {
-}
+//interface ReservationRepository extends ReactiveCrudRepository<Reservation, Integer> {
+//
+////  @Query("select * from reservation where name = $1 ")
+////  Flux<Reservation> findByName(String name);
+//}
 
 @Data
 @AllArgsConstructor
--- 8.0/readme.md
+++ 8.0/readme.md
@@ -3,4 +3,12 @@
 docker-compose up
 
 mvn spring-boot:run
+
+http://localhost:8080/reservations
+http://localhost:9999/proxy/reservations
+
+curl -v -H "Host: 7788.spring.io" http://localhost:9999/proxy
+curl -vu user:password -H "Host: 7788.spring.io" http://localhost:9999/proxy
+
+while true; do curl -vu user:password -H "Host: 7788.spring.io" http://localhost:9999/proxy; done
 ```
